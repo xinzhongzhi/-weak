94e89b0c6fc0a4bb1189cc37a568747beb5c93c4
risenb_xinzhongzhi <xinzz_sy_risenb@163.com>
2017-06-13 13:22:22 +0800

    Initial Commit

---
+++ 代理为什么用weak/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  代理为什么用weak
+//
+//  Created by 辛忠志 on 2017/6/13.
+//  Copyright © 2017年 辛忠志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 代理为什么用weak/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  代理为什么用weak
+//
+//  Created by 辛忠志 on 2017/6/13.
+//  Copyright © 2017年 辛忠志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
